97cc6ed51cb0a0728932be7e40de287fca445508
nathanmdaniel <nathan.melathel.daniel@gmail.com>
2019-07-02 14:37:46 -0500

    transactions decreasing the inventory stocks as well

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const bodyParser = require('body-parser');
 if(typeof require !== 'undefined') XLSX = require('xlsx');
 var inventory = XLSX.readFile('./SampleInventory.xlsx');
 
+/*
 var merchSheet = inventory.Sheets['Merchandise'];
 var snacksSheet = inventory.Sheets['Snacks'];
 var drinksSheet = inventory.Sheets['Drinks'];
 var frozenSheet = inventory.Sheets['Frozen'];
+*/
 
 var records = XLSX.readFile('./Records.xlsx');
 
@@ -27,6 +29,7 @@ function chargeBalance(name, amount) {
         if (name === account.Name) {
             account.Spent += amount;
             account.Balance -= amount;
+            // no break in JS foreach?
         }
     })
     //console.log("AFTER    ", balJson);
@@ -35,6 +38,41 @@ function chargeBalance(name, amount) {
     recSheet = records.Sheets['Sheet1'];
 
     XLSX.writeFile(records, 'Records.xlsx');
+}
+
+function decrementInventories(transactionItems) {
+    var merchJson = XLSX.utils.sheet_to_json(inventory.Sheets['Merchandise']);
+    var snacksJson = XLSX.utils.sheet_to_json(inventory.Sheets['Snacks']);
+    var drinksJson = XLSX.utils.sheet_to_json(inventory.Sheets['Drinks']);
+    var frozenJson = XLSX.utils.sheet_to_json(inventory.Sheets['Frozen']);
+    var catJson = null;
+    transactionItems.forEach(transactionItem => {
+        switch (transactionItem.sheet) {
+            case 'Merchandise':
+                catJson = merchJson;
+                break;
+            case 'Snacks':
+                catJson = snacksJson;
+                break;
+            case 'Drinks':
+                catJson = drinksJson;
+                break;
+            case 'Frozen':
+                catJson = frozenJson;
+                break;
+        }
+        catJson.forEach(inventoryItem => {
+            if (transactionItem.item === inventoryItem.Name) {
+                --inventoryItem.Stock;
+            }
+        })
+    })
+    inventory.Sheets['Merchandise'] = XLSX.utils.json_to_sheet(merchJson);
+    inventory.Sheets['Snacks'] = XLSX.utils.json_to_sheet(snacksJson);
+    inventory.Sheets['Drinks'] = XLSX.utils.json_to_sheet(drinksJson);
+    inventory.Sheets['Frozen'] = XLSX.utils.json_to_sheet(frozenJson);
+
+    XLSX.writeFile(inventory, 'SampleInventory.xlsx');
 }
 
 //Start the app
@@ -46,24 +84,28 @@ app.prepare()
     server.use(bodyParser.urlencoded({ extended: true }));
 
     server.get('/MerchJson', (req, res) => {
+        var merchSheet = inventory.Sheets['Merchandise'];
         const merchJSON = XLSX.utils.sheet_to_json(merchSheet)
         res.send(merchJSON)
         res.end()
         return res;
     })
     server.get('/SnacksJson', (req, res) => {
+        var snacksSheet = inventory.Sheets['Snacks'];
         const snacksJSON = XLSX.utils.sheet_to_json(snacksSheet)
         res.send(snacksJSON)
         res.end()
         return res;
     })
     server.get('/DrinksJson', (req, res) => {
+        var drinksSheet = inventory.Sheets['Drinks'];
         const drinksJSON = XLSX.utils.sheet_to_json(drinksSheet)
         res.send(drinksJSON)
         res.end()
         return res;
     })
     server.get('/FrozenJson', (req, res) => {
+        var frozenSheet = inventory.Sheets['Frozen'];
         const frozenJSON = XLSX.utils.sheet_to_json(frozenSheet)
         res.send(frozenJSON)
         res.end()
@@ -79,13 +121,27 @@ app.prepare()
         return handle(req, res)
     })
 
-    // DecrementInventories & Account's Balance
+    // Decrement Inventories & Account's Balance
     server.post('/DecInventories', (req, res) => {
         //console.log(req.body)
         chargeBalance(req.body.name, req.body.price);
+        decrementInventories(req.body.items);
         res.end();
         return res;
     })
+
+    // Add account to Records spreadsheet
+    server.post('/NewAccount', (req, res) => {
+        res.end();
+        return res;
+    })
+
+    // Increase balance of account in Records spreadsheet
+    server.post('/CreditAccount', (req, res) => {
+        res.end();
+        return res;
+    })
+
     server.listen(3001, (err) => {
         if (err) throw err
         console.log('> Ready on http://localhost:3001')
