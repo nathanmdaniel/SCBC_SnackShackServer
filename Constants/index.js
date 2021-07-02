2e9ee0cb03940473dfe2f8730272d1b363b28265
nathanmdaniel <nathan.melathel.daniel@gmail.com>
2021-07-02 15:19:00 -0500

    changed constants to match keith's comp

--- Constants/index.js
+++ Constants/index.js
@@ -1,3 +1,3 @@
 // Insert the IP Address of the device running your server.
 // export const SERVER_IP_ADDR = 'http://localhost:3001/';
-export const SERVER_IP_ADDR = 'http://192.168.1.21:3001/';
+export const SERVER_IP_ADDR = 'http://192.168.1.229:3001/';
--- Constants/server_ip_const.js
+++ Constants/server_ip_const.js
@@ -1,6 +1,6 @@
 // Insert the IP Address of the device running your server.
 // const ip_addr_const = 'http://localhost:3001/';
-const ip_addr_const = 'http://192.168.1.21:3001/';
+const ip_addr_const = 'http://192.168.1.229:3001/';
 
 
 module.exports = {
